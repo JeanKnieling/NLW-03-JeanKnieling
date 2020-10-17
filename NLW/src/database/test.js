2e54b4ef6a5090d338c4c4b2507e436f123717bb
JeanKnieling <64235932+JeanKnieling@users.noreply.github.com>
2020-10-17 01:49:21 -0300

    Avançando no código!!!

---
+++ NLW/src/database/test.js
@@ -0,0 +1,33 @@
+const Database = require('./db');
+
+Database.then(async db => {
+    // inserir dados na tabela
+    await db.run(`
+        INSERT INTO orphanages (
+            lat, 
+            lng,
+            name,
+            about,
+            whatsapp,
+            images,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        )  VALUES (
+            "-27.222633",
+            "-49.6455874",
+            "Lar das meninas",
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+            "999159950",
+            "https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+            "Horários de visitas das 18h até 8h",
+            "1"
+        );
+    `)
+
+    // consultar dados da tabela
+    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+    console.log(selectedOrphanages)
+
+})
