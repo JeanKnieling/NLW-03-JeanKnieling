837cca702ffc5d76efd1af7f061b866a97685e86
JeanKnieling <64235932+JeanKnieling@users.noreply.github.com>
2020-10-18 23:22:56 -0300

    Apenas mexendo!!!

--- NLW/src/database/test.js
+++ NLW/src/database/test.js
@@ -6,18 +6,18 @@ Database.then(async db => {
     await saveOrphanage(db, {
         lat: "-27.222633", 
         lng: "-49.6555874",
-        name: "Lar das meninos",
+        name: "Lar dos meninos",
         about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-        whatsapp: "999159950",
+        whatsapp: "999159915",
         images: [
-            "https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://unsplash.com/photos/4K2lIP0zc_k",
 
-            "https://images.unsplash.com/photo-1576024267263-70f1caffd6fe?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+            "https://unsplash.com/photos/4_mJ1TbMK8A"
         ].toString(),
 
         instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
         opening_hours: "Horários de visitas das 18h até 8h",
-        open_on_weekends: "0"
+        open_on_weekends: "1"
     })
 
     // consultar dados da tabela
@@ -25,10 +25,10 @@ Database.then(async db => {
     console.log(selectedOrphanages)
 
     // consultar somente 1 orphanato, pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
-    console.log(orphanage)
+    /*const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    console.log(orphanage)*/
 
-    // deletar dado da tabela
-   /* console.log(await db.run('DELETE FROM orphanages WHERE id = "4"'))
-    console.log(await db.run('DELETE FROM orphanages WHERE id = "5"'))*/
+    // deletar dados da tabela
+    /*console.log(await db.run('DELETE FROM orphanages WHERE id = "6"'))
+    /*console.log(await db.run('DELETE FROM orphanages WHERE id = "5"'))*/
 })
